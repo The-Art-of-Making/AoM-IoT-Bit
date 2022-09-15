4b46e6e08daf5b516dbdb8b7a7b2f23f15918ddf
delta-12 <btesq235@gmail.com>
2022-09-15 11:55:30 -0400

    add secrets header file for creds if SD card not working

---
+++ Source-Code/IoT-Bit-V3.0/secrets.h
@@ -0,0 +1,9 @@
+// If SD card reader on shield not working, place credentials here
+
+const String ssid = ""; // Your network SSID (name)
+const String pass = ""; // Your network password (use for WPA, or use as key for WEP)
+
+const String adafruit_io_username = "";  // The username attached to your Adafruit IO account
+const String adafruit_io_group_key = ""; // This is a unique key to a single group in Adafruit IO
+const String adafruit_io_feed_key = "";  // This is a unique key to a single feed in the above group
+const String adafruit_io_key = "";       // Unique key attached to it that serves as a security measure for sending data across WiFi and through the internet
