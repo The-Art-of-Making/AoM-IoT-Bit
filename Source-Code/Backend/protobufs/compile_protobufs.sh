40e1b4853c7113ef0f6f62882ae9da2bf6d4acee
delta-12 <btesq235@gmail.com>
2023-02-19 00:40:40 -0500

    update device action proto

--- Source-Code/Backend/protobufs/compile_protobufs.sh
+++ Source-Code/Backend/protobufs/compile_protobufs.sh
@@ -6,6 +6,7 @@ git submodule init
 echo "Compiling protobufs for python..."
 PYTHON_OUT_DIR=./python_out
 MQTT_CONTROLLER_PROTOBUFS=../mqtt-controller/application/protobufs
+MQTT_CLIENT_ACTIONS_PROTOBUFS=../mqtt-client-actions/application/protobufs
 if [ ! -d "$PYTHON_OUT_DIR" ]; then
     echo "Creating python_out directory"
     mkdir $PYTHON_OUT_DIR
@@ -14,10 +15,16 @@ if [ ! -d "$MQTT_CONTROLLER_PROTOBUFS" ]; then
     echo "Creating mqtt-controller/application/protobufs directory"
     mkdir $MQTT_CONTROLLER_PROTOBUFS
 fi
+if [ ! -d "$MQTT_CLIENT_ACTIONS_PROTOBUFS" ]; then
+    echo "Creating mqtt-client-actions/application/protobufs directory"
+    mkdir $MQTT_CLIENT_ACTIONS_PROTOBUFS
+fi
 protoc -I ./device --python_out=$PYTHON_OUT_DIR ./device/device.proto
 protoc -I ./controller -I ./device --python_out=$PYTHON_OUT_DIR ./controller/controller_message.proto
 rm -Rf $MQTT_CONTROLLER_PROTOBUFS/* && cp -r $PYTHON_OUT_DIR/* $MQTT_CONTROLLER_PROTOBUFS/
+rm -Rf $MQTT_CLIENT_ACTIONS_PROTOBUFS/* && cp -r $PYTHON_OUT_DIR/* $MQTT_CLIENT_ACTIONS_PROTOBUFS/
 sed -i "s/import device_pb2 as device__pb2/import protobufs.device_pb2 as device__pb2/g" $MQTT_CONTROLLER_PROTOBUFS/controller_message_pb2.py
+sed -i "s/import device_pb2 as device__pb2/import protobufs.device_pb2 as device__pb2/g" $MQTT_CLIENT_ACTIONS_PROTOBUFS/controller_message_pb2.py
 
 echo "Compiling protobufs for C (nanopb)..."
 C_OUT_DIR=./c_out
