e64a7947de87f554e9785e458de74123142c155d
delta-12 <btesq235@gmail.com>
2023-03-20 00:38:45 -0400

    add wifi creds to new client

--- Source-Code/Backend/mqtt-client-auth/validation/register_client.js
+++ Source-Code/Backend/mqtt-client-auth/validation/register_client.js
@@ -7,6 +7,8 @@ module.exports = function validateClient(data) {
     // Convert empty fields to an empty string to use validator functions
     data.user = !isEmpty(data.user) ? data.user : ""
     data.name = !isEmpty(data.name) ? data.name : ""
+    data.wifiSSID = !isEmpty(data.wifiSSID) ? data.wifiSSID : ""
+    data.wifiPassword = !isEmpty(data.wifiPassword) ? data.wifiPassword : ""
 
     // User ID check
     if (Validator.isEmpty(data.user)) {
@@ -16,6 +18,16 @@ module.exports = function validateClient(data) {
     // Name check
     if (Validator.isEmpty(data.name)) {
         errors.name = "Name is required"
+    }
+
+    // Wifi SSID check
+    if (Validator.isEmpty(data.wifiSSID)) {
+        errors.wifiSSID = "Wifi SSID is required"
+    }
+
+    // Wifi Password check
+    if (Validator.isEmpty(data.wifiPassword)) {
+        errors.wifiPassword = "Wifi Password is required"
     }
 
     return {
