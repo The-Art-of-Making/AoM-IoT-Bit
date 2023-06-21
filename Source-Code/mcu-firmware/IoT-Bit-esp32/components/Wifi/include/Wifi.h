9fc931acff5922a6e6ed8005d63dbef58700fbca
delta-12 <btesq235@gmail.com>
2023-06-20 23:38:17 -0400

    esp32 wifi test

---
+++ Source-Code/mcu-firmware/IoT-Bit-esp32/components/Wifi/include/Wifi.h
@@ -0,0 +1,10 @@
+#ifndef WIFI_H
+#define WIFI_H
+
+/* Function prototypes
+****************************************************************************************************/
+
+void Wifi_init(void);
+void Wifi_start(void);
+
+#endif
--- Source-Code/mcu-firmware/IoT-Bit-esp32/main/IoT-Bit-esp32.c
+++ Source-Code/mcu-firmware/IoT-Bit-esp32/main/IoT-Bit-esp32.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
+#include "Wifi.h"
 
 void app_main(void)
 {
-
+    Wifi_init();
+    Wifi_start();
 }
